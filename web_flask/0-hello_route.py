65ba3c2b3860c5904fde94cf92628bf931052d1d
Solomon Odunusi <solomonodunsui@gmail.com>
2023-12-31 05:28:42 -0800

    Debugged task 0 successfully

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Create a Flask app 
+"""Create a Flask app
 that listens on port 5000"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Display a Hello HBNB!"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
